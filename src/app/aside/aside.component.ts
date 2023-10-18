4e70c5b74d8dfc0a2fc3e12e399e5c968771031f
Kamil Chmielowski <kamil.chmielowski@comarch.com>
2023-10-18 21:20:32 +0200

    feat: aside layout

--- src/app/aside/aside.component.ts
+++ src/app/aside/aside.component.ts
@@ -1,10 +1,15 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
+import { SectionTitleComponent } from '../section-title/section-title.component';
+
 @Component({
   selector: 'app-aside',
   templateUrl: './aside.component.html',
   styleUrls: ['./aside.component.scss'],
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [
+    SectionTitleComponent
+  ]
 })
 export class AsideComponent {}
