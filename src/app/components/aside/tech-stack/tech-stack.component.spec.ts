6de41916c12f040cd8421e08a1e6b4cd6227bbf3
Kamil Chmielowski <kamil.chmielowski@comarch.com>
2024-01-22 09:13:12 +0100

    feat(tech-stack): unit tests

--- src/app/components/aside/tech-stack/tech-stack.component.spec.ts
+++ src/app/components/aside/tech-stack/tech-stack.component.spec.ts
@@ -1,8 +1,9 @@
+import { By } from '@angular/platform-browser';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TechStackComponent } from './tech-stack.component';
 
-xdescribe('TechStackComponent', () => {
+describe('TechStackComponent', () => {
   let component: TechStackComponent;
   let fixture: ComponentFixture<TechStackComponent>;
 
@@ -12,7 +13,17 @@ xdescribe('TechStackComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should create', () => expect(component).toBeTruthy());
+
+  it('should render min 5 tech bars', () => {
+    const progressBarsEl = fixture.debugElement.queryAll(By.css('app-progress-bar'))
+
+    expect(progressBarsEl.length).toBeGreaterThanOrEqual(5);
+  });
+
+  it('should pass content to app-progress-bar components', () => {
+    const elements = fixture.debugElement.queryAll(By.css('app-progress-bar'));
+
+    elements.forEach(element => expect(element.nativeElement.textContent).toBeTruthy());
   });
 });
