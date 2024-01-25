9a690bbe6218d2b404625d18a377788d416042c0
Kamil Chmielowski <kamil.chmielowski@comarch.com>
2024-01-25 13:05:56 +0100

    feat: nav mobile resolutions

--- src/app/components/projects/projects.component.spec.ts
+++ src/app/components/projects/projects.component.spec.ts
@@ -1,5 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
+import { JasmineUtil } from '../../utils/jasmine.util';
 import { ProjectsComponent } from './projects.component';
 
 describe('ProjectsComponent', () => {
@@ -7,7 +9,12 @@ describe('ProjectsComponent', () => {
   let fixture: ComponentFixture<ProjectsComponent>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [JasmineUtil.moduleWithTranslations([
+        ProjectsComponent,
+      ])],
+      providers: [JasmineUtil.svgIconSpyProvider()],
+    });
 
     fixture = TestBed.createComponent(ProjectsComponent);
     component = fixture.componentInstance;
--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,5 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { SectionTitleComponent } from '../home/section-title/section-title.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-projects',
@@ -7,6 +9,10 @@ import { RouterLink } from '@angular/router';
   styleUrls: ['./projects.component.scss'],
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [RouterLink,],
+  imports: [
+    RouterLink,
+    SectionTitleComponent,
+    TranslateModule,
+  ],
 })
 export class ProjectsComponent {}
